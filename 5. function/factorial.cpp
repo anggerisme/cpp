ae3f1eb6f47a26407a442f9c17838c37e935b235
Angger Bayu Pangestu <kontak@angger.me>
2021-05-19 10:00:56 +0700

    factorial

--- 5. function/factorial.cpp
+++ 5. function/factorial.cpp
@@ -3,24 +3,22 @@ using namespace std;
 
 
 int rekursif(int n){
+    /* Kondisi terminasi */
     if(n <= 1){
         cout << n << " = ";
         return n ;
     }
+    /* Fungsi Factorial */
     else
         cout << n << " * ";
         return n*rekursif(n - 1);
-    // else{
-        
-    // }
 }
 
 int main(){
     int n;
-    cout << "Masukkan bilangan bulat :\n";
+    cout << "Masukkan bilangan bulat : ";
     cin >> n;
     cout << n << " Faktorial adalah :\n";
-
     cout << rekursif(n) << "\n";
 }
 
